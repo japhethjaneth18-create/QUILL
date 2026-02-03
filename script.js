76c6d867e9f33d7f5958262b32744d81fe8ad015
japhethjaneth18-create <japhethjaneth18@gmail.com>
2026-02-03 18:20:07 +0300

    i changed overflow in eg.css, added some JavaScript and created a personalised journal space.

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function openJournal(category) {
 function closeJournal() {
   document.getElementById("journalArea").classList.add("hidden");
 }
+
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+function openSidebar() {
+  document.getElementById("sidebar").classList.add("active");
+  document.getElementById("overlay").classList.add("active");
+}
+
+function closeSidebar() {
+  document.getElementById("sidebar").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
+}
+
